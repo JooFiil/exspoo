7504028f9614d04ca265deb4e655a0f912cbeef6
Jorge Pinheiro <jorgefcpinheiro.2002@gmail.com>
2021-03-15 18:47:22 +0000

    .

---
+++ ex001.1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+	string nome;
+	int idade;
+	cout << "Qual e o seu nome?: "; cin >> nome;
+	cout << "E qual a sua idade?: "; cin >> idade;
+	cout << "\nOla, " << nome << "! Voce tem " << idade << " anos";
+}
